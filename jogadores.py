ebf672ed2d19617528009d7012281ae25ae460b3
ArthurRufi <temparthur87@gmail.com>
2023-04-10 16:05:20 -0300

    Criaçao do modulo jogadores.py e inicio de classes.

---
+++ jogadores.py
@@ -0,0 +1,19 @@
+class Jogador:
+    def __init__(self, nome, nascimento, notas):
+        self._nome =  nome
+        self._nascimento = nascimento
+        self._notas= notas
+        pass
+
+
+class Pontos():
+    def __init__(self, tops, cabecapoints, pontosproprios):
+        self._toptree = tops
+        self._cabeca = cabecapoints
+        self._pontos = pontosproprios
+        pass
+
+
+class Habilidades():
+    def __init__(self) -> None:
+        pass
